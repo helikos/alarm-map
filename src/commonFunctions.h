4bed7058e04b33948f23d060516217ebf627116b
Tymur Malyhin <tymur.malygin@gmail.com>
2023-08-22 09:57:26 +0300

    initial

---
+++ src/commonFunctions.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef _COMMONFUNCTION_H
+#define _COMMONFUNCTION_H
+
+void initStrip();
+void noneStrip();
+void colorWipe();
+void testMap();
+void getFreeHeapSize(AsyncWebServerRequest *request);
+
+
+#endif
