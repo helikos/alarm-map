5bf6438cc7dc58a78ff3cfeec35e63bf129bcf35
Tymur Malyhin <tymur.malygin@gmail.com>
2023-08-22 10:19:32 +0300

    rr

---
+++ src/configuration.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef _CONFIGURATION_H
+#define _CONFIGURATION_H
+
+
+#define isPrint2Serial 
+#define debug
+
+#define LED_PIN 13
+#define LED_COUNT 25
+#define sizeOfLog 128
+#define msgLength 64
+#define ledBrightness 100
+
+
+
+#endif
+
